db6b091570bf83fd5d1c9d927edd4343af0f1e05
diassmith <diassmith>
2022-08-11 23:35:07 +0000

    joins
    doing some joins between datas

--- 2 - silver - to - gold/race_results.py
+++ 2 - silver - to - gold/race_results.py
@@ -3,11 +3,19 @@
 
 # COMMAND ----------
 
+from pyspark.sql.functions import current_timestamp
+
+# COMMAND ----------
+
 # DBTITLE 1,Reading drivers file and renaming some column
 df_drivers = spark.read.parquet(f"{silver_folder_path}/drivers") \
 .withColumnRenamed("number", "driver_number") \
 .withColumnRenamed("name", "driver_name") \
 .withColumnRenamed("nationality", "driver_nationality") 
+
+# COMMAND ----------
+
+display(df_drivers)
 
 # COMMAND ----------
 
@@ -17,9 +25,17 @@ df_constructors = spark.read.parquet(f"{silver_folder_path}/constructors") \
 
 # COMMAND ----------
 
+display(df_constructors)
+
+# COMMAND ----------
+
 # DBTITLE 1,Reading circuits file and renaming column
 df_circuits = spark.read.parquet(f"{silver_folder_path}/circuits") \
 .withColumnRenamed("location", "circuit_location") 
+
+# COMMAND ----------
+
+display(df_circuits)
 
 # COMMAND ----------
 
@@ -30,14 +46,51 @@ df_races = spark.read.parquet(f"{silver_folder_path}/races") \
 
 # COMMAND ----------
 
+display(df_races)
+
+# COMMAND ----------
+
 # DBTITLE 1,Reading Results file and renaming column
 df_results = spark.read.parquet(f"{silver_folder_path}/results") \
 .withColumnRenamed("time", "race_time") 
 
 # COMMAND ----------
 
-display(df_circuits)
+display(df_results)
 
 # COMMAND ----------
 
-display(df_races)
+# DBTITLE 1,Join Race and Circuits
+df_races_circuits = df_races.join(df_circuits, df_races.circuit_id == df_circuits.circuit_id, "inner") \
+.select(df_races.race_id, df_races.race_year, df_races.race_name, df_races.race_date, df_circuits.circuit_location)
+
+# COMMAND ----------
+
+display(df_races_circuits)
+
+# COMMAND ----------
+
+# DBTITLE 1,Join Results to all dataframes 
+df_race_results = df_results.join(df_races_circuits, df_results.race_id == df_races_circuits.race_id) \
+                            .join(df_drivers, df_results.driver_id == df_drivers.driver_id) \
+                            .join(df_constructors, df_results.constructor_id == df_constructors.constructor_id)
+
+# COMMAND ----------
+
+df_final = df_race_results.select("race_year", "race_name", "race_date", "circuit_location", "driver_name", "driver_number", "driver_nationality",
+                                 "team", "grid", "fastest_lap", "race_time", "points", "position")\
+                                .withColumn("created_date", current_timestamp())
+
+# COMMAND ----------
+
+display(df_final)
+
+# COMMAND ----------
+
+# DBTITLE 1,Creating table igual BBC website
+display(df_final.filter("race_year == 2020 and race_name == 'Abu Dhabi Grand Prix'").orderBy(df_final.points.desc()))
+
+# COMMAND ----------
+
+# DBTITLE 1,Write output in Gold Layer
+df_final.write.mode("overwrite").parquet(f"{gold_folder_path}/race_results")
