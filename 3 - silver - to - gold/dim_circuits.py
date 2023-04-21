2fdc36bd737a445d611bc3ddd4566eed80b70593
diassmith <diassmith>
2023-04-21 16:58:42 +0000

    DimCircuits

---
+++ 3 - silver - to - gold/dim_circuits.py
@@ -0,0 +1,81 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### Working with circuits.csv file
+
+# COMMAND ----------
+
+from delta.tables import *
+
+# COMMAND ----------
+
+# DBTITLE 1,Run the configuration notebook 
+# MAGIC %run "../0 - includes/configuration"
+
+# COMMAND ----------
+
+# DBTITLE 1,Run the functions notebook 
+# MAGIC %run "../0 - includes/functions"
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Reading the CSV file using the spark dataframe reader
+
+# COMMAND ----------
+
+# DBTITLE 1,Importing Library
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DoubleType
+from pyspark.sql.functions import col, lit
+
+# COMMAND ----------
+
+# DBTITLE 1,Reading the file
+df_circuits = spark.table("f1_silver.circuits")
+
+# COMMAND ----------
+
+df_circuits.printSchema()
+
+# COMMAND ----------
+
+display(df_circuits)
+
+# COMMAND ----------
+
+# DBTITLE 1,Renaming the columns
+df_circuits = (df_circuits.withColumnRenamed("circuit_id", "IdCircuit")
+                                            .withColumnRenamed("circuit_ref", "Reference")
+                                            .withColumnRenamed("name", "Name")
+                                            .withColumnRenamed("location", "Location")
+                                            .withColumnRenamed("country", "Country")
+                                            .withColumnRenamed("latitude", "Latitude")
+                                            .withColumnRenamed("longitude", "Longitude")
+                                            .withColumnRenamed("altitude", "Altitude"))
+
+# COMMAND ----------
+
+df_circuits = df_circuits.drop(df_circuits.file_date).drop(df_circuits.date_load_silver)
+
+# COMMAND ----------
+
+# df_circuits.show()
+
+# COMMAND ----------
+
+# DBTITLE 1,Write dim Circuits
+if spark.catalog.tableExists("f1_gold.dim_Circuits"):
+    df_target = DeltaTable.forPath(spark, f"{gold_folder_path}"+"/dim_Circuits")
+    print("upsert")
+    upsert(df_target,IdCircuit,df_circuits,IdCircuit)
+else:
+    print("New")
+    df_circuits.write.mode("overwrite").format("delta").saveAsTable("f1_gold.dim_Circuits")
+
+# COMMAND ----------
+
+dbutils.notebook.exit("Sucess")
+
+# COMMAND ----------
+
+# %sql
+# SELECT * FROM f1_gold.dim_circuits
