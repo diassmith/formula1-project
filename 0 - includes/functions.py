fdd16089c963a94bdc3d234e6468ee3a6f3cd77e
diassmith <diassmith>
2023-04-25 01:17:33 +0000

    ingestion

--- 0 - includes/functions.py
+++ 0 - includes/functions.py
@@ -1,5 +1,12 @@
 # Databricks notebook source
 from pyspark.sql.functions import current_timestamp
+def add_date_load_landing(input_df):
+  df_output = input_df.withColumn("date_ref", current_timestamp())
+  return df_output
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp
 def add_date_load_bronze(input_df):
   df_output = input_df.withColumn("date_load_bronze", current_timestamp())
   return df_output
