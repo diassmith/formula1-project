e6679d19ce428c5d25603704577287b490c902ca
diassmith <diassmith>
2023-04-29 13:29:00 +0000

    updates

--- 02 - landing - to - bronze/drivers - Bronze.py
+++ 02 - landing - to - bronze/drivers - Bronze.py
@@ -40,7 +40,7 @@ df_drivers = (add_date_load_bronze(df_drivers))
 
 # COMMAND ----------
 
-df_drivers = df_drivers.select('code','dateOfBirth','driverId','familyName','givenName','nationality','permanentNumber','url','year','date_load_bronze','SkDrivers')
+df_drivers = df_drivers.select('id','code','dateOfBirth','driverId','familyName','givenName','nationality','permanentNumber','url','year','date_load_bronze')
 
 # COMMAND ----------
 
@@ -51,7 +51,7 @@ display(df_drivers)
 if spark.catalog.tableExists("f1_bronze.drivers"):
     df_target = DeltaTable.forPath(spark, f"{bronze_folder_path}"+"/drivers")
     print("upsert")
-    upsert(df_target,"Skdrivers",df_drivers,"Skdrivers")
+    upsert(df_target,"id",df_drivers,"id")
 else:
     print("New")
     df_drivers.write.mode("overwrite").format("delta").saveAsTable("f1_bronze.drivers")
