44812e2ae63faac8b38e8146c8cc9284e907287d
diassmith <diassmith>
2023-05-01 04:32:49 +0000

    Updating Drivers Silver

--- 03 - bronze - to - silver/drivers - Silver.py
+++ 03 - bronze - to - silver/drivers - Silver.py
@@ -1,10 +1,6 @@
 # Databricks notebook source
 # MAGIC %md
-# MAGIC ### Working drivers file
-
-# COMMAND ----------
-
-from delta.tables import *
+# MAGIC ### Drivers Silver
 
 # COMMAND ----------
 
@@ -18,25 +14,7 @@ from delta.tables import *
 
 # COMMAND ----------
 
-# DBTITLE 1,Importing Libraries and Functions
-from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DateType
-from pyspark.sql.functions import col, concat, lit
-
-# COMMAND ----------
-
-# # df_drivers = spark.read\
-# # .schema(drivers_schema)\
-# # .json(f"{landing_folder_path}/drivers.json")
-
-# df_drivers = spark.read.parquet(f"{bronze_folder_path}/drivers")
-
-# COMMAND ----------
-
 df_drivers = spark.table("f1_bronze.drivers")
-
-# COMMAND ----------
-
-display(df_drivers)
 
 # COMMAND ----------
 
@@ -55,20 +33,12 @@ df_drivers = (df_drivers.withColumnRenamed("driverId", "driverRef")
 
 # COMMAND ----------
 
-display(df_drivers)
-
-# COMMAND ----------
-
 # DBTITLE 1,Creating new column
 df_drivers = add_date_load_silver(df_drivers)
 
 # COMMAND ----------
 
-display(df_drivers)
-
-# COMMAND ----------
-
-# DBTITLE 1,Write output on parquet file in processed layer
+# DBTITLE 1,Creating Drivers Delta Table 
 if spark.catalog.tableExists("f1_silver.drivers"):
     df_target = DeltaTable.forPath(spark, f"{silver_folder_path}"+"/drivers")
     print("upsert")
